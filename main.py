d726cd789e14fa18e8a7416bb474104dfb1e437e
Piyush1824 <piyushshinde42161@gmail.com>
2026-01-02 11:00:15 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,82 @@
+# facial recognition attendance
+
+# pip install cmake
+# pip install face_recognition
+# pip install numpy
+# pip install opencv-python
+
+import face_recognition
+import cv2
+import numpy as np
+import csv
+from datetime import datetime
+
+video_capture = cv2.VideoCapture(0)
+
+# Load known images from stored images
+piyush_image = face_recognition.load_image_file("faces/Piyush.jpg")
+piyush_encoding = face_recognition.face_encodings(piyush_image)[0]
+
+# ajit_image = face_recognition.load_image_file("faces/ajit.jpeg")
+# ajit_encoding = face_recognition.face_encodings(ajit_image)[0]
+
+# vighnesh_image = face_recognition.load_image_file("faces/vighnesh.jpeg")
+# vighnesh_encoding = face_recognition.face_encodings(vighnesh_image)[0]
+
+known_face_encodings = [piyush_encoding]    # ajit_encoding, vighnesh_encoding]
+known_face_names = ["piyush", "ajit", "vighnesh"]
+
+# List of expected students
+students = known_face_names.copy()
+
+face_locations = []
+face_encodings = []
+
+# Get the current date and time
+now = datetime.now()
+current_date = now.strftime("%d-%m-%Y")
+
+f = open(f"{current_date}.csv", "w+", newline="")
+lnwriter = csv.writer(f)
+
+while True:
+    _, frame = video_capture.read()
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
+    # Recognize faces captured by webcam
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+    for face_encoding in face_encodings:
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+        # check the accuracy of captured image with stored image
+        face_distance = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distance)
+
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+
+        # Add the text if a person is present
+        if name in known_face_names:
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            bottomLeftCornerOfText = (10, 100)
+            fontScale = 1.5
+            fontColor = (255, 0, 0)
+            thickness = 4
+            lineType = 3
+            cv2.putText(frame, name + "Present", bottomLeftCornerOfText, font, fontScale, fontColor, thickness, lineType)
+
+            if name in students:
+                students.remove(name)
+                current_time = now.strftime("%H:%M")
+                lnwriter.writerow([name, current_time, current_date])
+
+    cv2.imshow("Attendance", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
+f.close()
